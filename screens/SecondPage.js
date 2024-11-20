06650c82f4a0aa94d306d6341a542184938d76ff
taks-mohs <taks.compsci@gmail.com>
2024-11-20 08:20:09 -1000

    change page 2

--- screens/SecondPage.js
+++ screens/SecondPage.js
@@ -1,12 +1,19 @@
 import { StyleSheet, Text } from "react-native";
 function SecondPage() {
   return (
-    <Text>Second Page</Text>
+    <View>
+      <Text>Second Page</Text>
+      <Text>Change #1</Text>
+      <Text>Change #2</Text>
+    </View>
+    
   );
 }
 
 export default SecondPage;
 
 const styles = StyleSheet.create({
-
+  container: {
+    
+  }
 });
