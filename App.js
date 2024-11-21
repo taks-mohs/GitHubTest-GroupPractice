598ca4a2b0e978673adee11b86be5b630c635ae4
taks-mohs <taks.compsci@gmail.com>
2024-11-20 16:33:20 -1000

    button to go to page 2 works

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
 import { StyleSheet, SafeAreaView } from 'react-native';
+import { useState } from 'react';
 
 import FirstPage from './screens/FirstPage';
+import SecondPage from './screens/SecondPage';
+
+
 
 export default function App() {
-  let screen = <FirstPage />
+  const [pageNum, setPageNum] = useState(0);
+
+  function changePageHandler() {
+    setPageNum(1);
+  }
+
+  let screen = <FirstPage whenPressed={changePageHandler}/>
+
+  if(pageNum === 1) {
+    screen = <SecondPage />
+  }
 
   return (
     <SafeAreaView style={styles.container}>
